f0afaee05377c637328f520ee50780ad77401831
miguangying <miguangying@01zhuanche.com>
2017-03-02 19:18:38 +0800

    service-customer测试类

---
+++ mifuns-service-customer/src/main/java/com/mifuns/customer/service/test/TestServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mifuns.customer.service.test;
+
+import com.mifuns.customer.facade.test.TestService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by miguangying on 2017/3/1.
+ */
+@Service("testService")
+public class TestServiceImpl implements TestService{
+    @Override
+    public String insertService() {
+        return "OK";
+    }
+}
